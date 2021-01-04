183e0e880aed192abb45ea52dc827d01460b7b03
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-01-05 01:24:45 +0200

    Добавлены решения задачь с 1 по 4

---
+++ js/js-hw-autocheck-m1.js
@@ -0,0 +1,34 @@
+// Задача 1/36
+
+const productName = 'Дроид';
+const pricePerItem = 2000;
+  
+console.log(productName);
+console.log(pricePerItem);
+
+
+// Задача 2/36
+
+let productName = 'Дроид';
+let pricePerItem = 2000;
+
+productName = 'Ремонтный дроид';
+pricePerItem = 3500;
+
+
+// Задача 3/36
+
+const topSpeed = 160;
+const distance = 617.54;
+const login = 'mango935';
+const isOnline = true;
+const isAdmin = false;
+
+
+// Задача 4/36
+
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+const totalPrice = pricePerItem * orderedQuantity;
+
