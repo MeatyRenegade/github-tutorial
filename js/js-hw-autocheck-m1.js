c6855b0521e7e5302c7df6650ecec315cbe8b67e
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-01-05 02:43:24 +0200

    Update js-hw-autocheck-m1.js

--- js/js-hw-autocheck-m1.js
+++ js/js-hw-autocheck-m1.js
@@ -32,3 +32,20 @@ const orderedQuantity = 4;
 
 const totalPrice = pricePerItem * orderedQuantity;
 
+
+// Задача 5/36
+
+const productName = 'Дроид';
+const pricePerItem = 3500;
+
+const message = `Вы выбрали ${productName}, цена за штуку ${pricePerItem} кредитов`;
+
+
+// Задача 6/36
+
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+
+const message = `Вы заказали дроидов на сумму ${totalPrice} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
