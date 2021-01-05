f61709ef1e529fe03e8a81afef1faf4a73c78b21
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-01-06 01:31:19 +0200

    Update js-hw-autocheck-m1.js

--- js/js-hw-autocheck-m1.js
+++ js/js-hw-autocheck-m1.js
@@ -1,5 +1,4 @@
 // Задача 1/36
-
 const productName = 'Дроид';
 const pricePerItem = 2000;
   
@@ -8,7 +7,6 @@ console.log(pricePerItem);
 
 
 // Задача 2/36
-
 let productName = 'Дроид';
 let pricePerItem = 2000;
 
@@ -17,7 +15,6 @@ pricePerItem = 3500;
 
 
 // Задача 3/36
-
 const topSpeed = 160;
 const distance = 617.54;
 const login = 'mango935';
@@ -26,7 +23,6 @@ const isAdmin = false;
 
 
 // Задача 4/36
-
 const pricePerItem = 3500;
 const orderedQuantity = 4;
 
@@ -34,7 +30,6 @@ const totalPrice = pricePerItem * orderedQuantity;
 
 
 // Задача 5/36
-
 const productName = 'Дроид';
 const pricePerItem = 3500;
 
@@ -42,10 +37,42 @@ const message = `Вы выбрали ${productName}, цена за штуку ${
 
 
 // Задача 6/36
-
 const pricePerDroid = 800;
 const orderedQuantity = 6;
 const deliveryFee = 50;
 const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
 
 const message = `Вы заказали дроидов на сумму ${totalPrice} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+
+
+// Задача 7/36
+function sayHi() {  
+  console.log('Привет, это моя первая функция!');
+}
+
+sayHi();
+ 
+
+// Задача 8/36
+function add(a,b,c) {
+  console.log(`Результат сложения равен ${a+b+c}`);  
+}
+  
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
+
+
+// Задача 9/36
+
+function makeMessage (name, price) {
+  // Пиши код ниже этой строки
+  makeMessage('Радар', 6150);
+  makeMessage('Сканер', 3500);
+  makeMessage('Реактор', 8000);
+  makeMessage('Двигатель', 4070);
+   const message = `Вы выбрали @{makeMessage}, цена за штуку @{price} кредитов`;
+  
+  // Пиши код выше этой строки
+  return message;
+};
